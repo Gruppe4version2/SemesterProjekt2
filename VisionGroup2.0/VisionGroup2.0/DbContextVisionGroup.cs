9732ddea6f34646547d1c9bc6052810877a9b578
Mads Meinert Andersen <mads485d@edu.easj.dk>
2018-12-03 13:51:19 +0100

    Til Henne

--- VisionGroup2.0/VisionGroup2.0/DbContextVisionGroup.cs
+++ VisionGroup2.0/VisionGroup2.0/DbContextVisionGroup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using VisionGroup2._0.DomainClasses;
 
 namespace VisionGroup2._0
 {
@@ -15,7 +16,10 @@ namespace VisionGroup2._0
         {
         }
 
+        public virtual DbSet<Costumer> Costumers { get; set; }
+        public virtual DbSet<Employee> Employees { get; set; }
         public virtual DbSet<Project> Projects { get; set; }
+        public virtual DbSet<ProjectsForEmployee> ProjectsForEmployees { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -28,6 +32,46 @@ namespace VisionGroup2._0
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Costumer>(entity =>
+            {
+                entity.ToTable("Costumer");
+
+                entity.Property(e => e.CostumerId)
+                    .HasColumnName("Costumer_Id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.CvrNr).HasColumnName("CVR_nr");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.PhoneNr).HasColumnName("Phone_Nr");
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.EmployeeId)
+                    .HasColumnName("Employee_Id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.PhoneNr).HasColumnName("Phone_Nr");
+            });
+
             modelBuilder.Entity<Project>(entity =>
             {
                 entity.ToTable("Project");
@@ -43,6 +87,35 @@ namespace VisionGroup2._0
                 entity.Property(e => e.Name)
                     .IsRequired()
                     .HasMaxLength(50);
+
+                entity.HasOne(d => d.Costumer)
+                    .WithMany(p => p.Projects)
+                    .HasForeignKey(d => d.CostumerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_CostumerID");
+            });
+
+            modelBuilder.Entity<ProjectsForEmployee>(entity =>
+            {
+                entity.HasKey(e => new { e.ProjectId, e.EmployeeId });
+
+                entity.Property(e => e.ProjectId).HasColumnName("Project_Id");
+
+                entity.Property(e => e.EmployeeId).HasColumnName("Employee_Id");
+
+                entity.Property(e => e.IsLeader).HasColumnName("Is_Leader");
+
+                entity.HasOne(d => d.Employee)
+                    .WithMany(p => p.ProjectsForEmployees)
+                    .HasForeignKey(d => d.EmployeeId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_EmployeeId");
+
+                entity.HasOne(d => d.Project)
+                    .WithMany(p => p.ProjectsForEmployees)
+                    .HasForeignKey(d => d.ProjectId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_ProjectId");
             });
 
             OnModelCreatingPartial(modelBuilder);
